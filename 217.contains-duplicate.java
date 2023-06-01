1eb514f5712f4bcfa36a947d513338e183d1f9af
dipyamandeb <dipyamandeb19@gmail.com>
2023-06-01 13:33:47 +0530

    Done

---
+++ 217.contains-duplicate.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=217 lang=java
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for(int i = 0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
+// @lc code=end
+
