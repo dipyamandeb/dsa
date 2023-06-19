75d38c28a328cebbf0bf85ec1edd779c88298048
dipyamandeb <dipyamandeb19@gmail.com>
2023-06-19 13:40:17 +0530

    Two-sum

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -11,20 +11,19 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> hm = new HashMap();
-        for(int i = 0;i<nums.length;i++){
-            if(hm.containsKey(target-nums[i])){
+        Map<Integer, Integer> hm = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
                 int[] res = new int[2];
-                res[0]=hm.get(target-nums[i]);
-                res[1]=i;
+                res[0] = hm.get(target - nums[i]);
+                res[1] = i;
                 return res;
             }
-            hm.put(nums[i],i);
-            
-        }     
-           return null;
-       } 
-   
+            hm.put(nums[i], i);
+
+        }
+        return null;
+    }
+
 }
 // @lc code=end
-
